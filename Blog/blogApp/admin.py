a8d50ae0bd59482905a838d86ffc27afb1342e0e
Guilherme <cavalcante.guilherme91@gmail.com>
2018-05-12 11:14:28 -0300

    My blog project from linkedin lessons.

---
+++ Blog/blogApp/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from .models import Post
+# Register your models here.
+# Customizing the admin site
+
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'slug', 'author', 'publish', 'status')
+    list_filter = ('status', 'created', 'publish', 'author')
+    prepopulated_fields = {'slug': ('title',)}
+    raw_id_fields = ('author',)
+    date_hierarchy = 'publish'
+    ordering = ['status', 'publish']
+
+
+admin.site.register(Post, PostAdmin)
